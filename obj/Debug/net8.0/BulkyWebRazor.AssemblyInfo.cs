de9727df939199e44c7d2060e7a5726b78c6438d
Asadujjaman Turag <asadujjaman717@gmail.com>
2025-05-25 14:19:08 +0600

    initially

--- obj/Debug/net8.0/BulkyWebRazor.AssemblyInfo.cs
+++ obj/Debug/net8.0/BulkyWebRazor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyWebRazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+412ce019b7e82f83ab75f5ae7c4f5fb4f95e57b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyWebRazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyWebRazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
